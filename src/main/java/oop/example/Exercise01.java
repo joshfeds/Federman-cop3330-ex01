3531e0a52c3ec5b01db5f00a3a277ca949703319
joshfeds14 <joshfeds14@gmail,com>
2021-06-04 16:16:22 -0400

    Exercise 01

--- src/main/java/oop/example/Exercise01.java
+++ src/main/java/oop/example/Exercise01.java
@@ -1,6 +1,9 @@
 package oop.example;
 import java.util.Scanner;
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Federman
+ */
 public class Exercise01 {
     public static void main(String[] args) {
 
@@ -8,8 +11,7 @@ public class Exercise01 {
         System.out.print("What is your name?");
 
         String name = input.next();
-        System.out.print("Hello, "+name);
-        System.out.print(", nice to meet you!");
+        System.out.printf("Hello, %s, nice to meet you", name);
 
     }
 }
